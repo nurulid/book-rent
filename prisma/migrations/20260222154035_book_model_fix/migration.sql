9599381938309d1684acb4abcbe90955e9c4fe70
Nurul Insani Dewi <nurulinsanid@gmail.com>
2026-02-22 23:35:18 +0700

    tweaks schema

---
+++ prisma/migrations/20260222154035_book_model_fix/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `updateAt` on the `Book` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `Book` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Book" DROP COLUMN "updateAt",
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
